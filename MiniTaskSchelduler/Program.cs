9a5cdfe610f769814b6cb9ae0f5cf540884deb3b
Vada200 <vadahiv2019@gmail.com>
2026-09-08 13:31:03 +0200

    define scheduling logic

--- MiniTaskSchelduler/Program.cs
+++ MiniTaskSchelduler/Program.cs
@@ -4,19 +4,36 @@ namespace MiniTaskSchelduler
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             List<ScheduledTask> tasks = new List<ScheduledTask>();
             tasks.Add(new ScheduledTask("Creating backup", 10));
-            tasks.Add(new ScheduledTask("Testing", 10));
-            tasks.Add(new ScheduledTask("Executing", 10));
+            tasks.Add(new ScheduledTask("Testing", 15));
+            tasks.Add(new ScheduledTask("Executing", 20));
 
             foreach (var task in tasks)
             {
                 Console.WriteLine($"Task: {task.Name}, in every {task.IntervalSeconds} seconds, next run: {task.NextRun}");
+                
             }
+            await RunLoopAsync(tasks);
 
+        }
 
+        private static async Task RunLoopAsync(List<ScheduledTask> tasks)
+        {
+            while (true)
+            {
+                foreach (var task in tasks)
+                {
+                    if (task.NextRun <= DateTime.Now)
+                    {
+                        Console.WriteLine($"Task ended: {task.Name}");
+                        task.LastRun = DateTime.Now;
+                    }
+                }
+                await Task.Delay(1000);
+            }
         }
     }
 }
