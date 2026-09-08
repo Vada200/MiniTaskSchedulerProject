cdeed6ee12454c3c6744fd112e6b664b47e3542b
Vada200 <vadahiv2019@gmail.com>
2026-09-08 13:18:49 +0200

    Initializing hardcoded tasks

---
+++ MiniTaskSchelduler/Models/ScheduledTask.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiniTaskSchelduler.Models
+{
+    internal class ScheduledTask
+    {
+        public Guid Id { get; private set; }
+        public string Name { get; set; }
+        public int IntervalSeconds { get; set; }
+
+        public DateTime? LastRun { get; set; } //? - nullable
+
+        public DateTime NextRun
+        {
+            get {
+                if (LastRun.HasValue)
+                    return LastRun.Value.AddSeconds(IntervalSeconds);
+                else
+                    return DateTime.Now;
+            }
+        }
+
+        public ScheduledTask( string name, int intervalSeconds)
+        {
+            this.Id = Guid.NewGuid();
+            this.Name = name;
+            this.IntervalSeconds = intervalSeconds;
+            this.LastRun = null;
+        }
+    }
+}
---
+++ MiniTaskSchelduler/Program.cs
@@ -0,0 +1,22 @@
+﻿using MiniTaskSchelduler.Models;
+
+namespace MiniTaskSchelduler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<ScheduledTask> tasks = new List<ScheduledTask>();
+            tasks.Add(new ScheduledTask("Creating backup", 10));
+            tasks.Add(new ScheduledTask("Testing", 10));
+            tasks.Add(new ScheduledTask("Executing", 10));
+
+            foreach (var task in tasks)
+            {
+                Console.WriteLine($"Task: {task.Name}, in every {task.IntervalSeconds} seconds, next run: {task.NextRun}");
+            }
+
+
+        }
+    }
+}
